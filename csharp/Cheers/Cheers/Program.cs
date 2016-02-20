00c961799df04a82cae6000f2d6523b1bd5eaa5b
Leif Hetland <lehetland1@gmail.com>
2016-02-20 10:15:21 -0600

    asking user for name and displaying input

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -10,6 +10,15 @@ namespace Cheers
     {
         static void Main(string[] args)
         {
+            // Prompt the user for their name
+            Console.Write("What is your name or type \"quit\" to exit: ");
+
+            string entry = Console.ReadLine();
+
+
+            // Display user name
+            Console.WriteLine("You're name is " + entry);
+
         }
     }
 }
