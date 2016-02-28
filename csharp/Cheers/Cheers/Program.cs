053b20c0273bc5b0140c17dc401a0e9dcdc52964
Leif Hetland <lehetland1@gmail.com>
2016-02-27 18:32:58 -0600

    happy birthday works, 2016 works, working on 2017 birthdays then done

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -11,13 +11,45 @@ namespace Cheers
         static void Main(string[] args)
         {
             // Prompt the user for their name
-            Console.Write("What is your name or type \"quit\" to exit: ");
+            Console.Write("What is your name? ");
 
             string entry = Console.ReadLine();
+            char[] array = entry.ToCharArray();
+            string vowels = "halfnorsemix";
+            for (int i = 0; i < array.Length; i++)
+            {
+                char letter = array[i];
+                if (vowels.Contains(letter))
+                {
+                    Console.WriteLine("Give me an " + letter + "..." + letter);
+                }
+                else
+                {
+                    Console.WriteLine("Give me a " + letter + "..." + letter);
+                }
+            }
+            Console.Write(entry.ToUpper() + " is... GRAND!" + "\r\n");
+            
 
-
-            // Display user name
-            Console.WriteLine("You're name is " + entry);
+            Console.Write("What is your birthday?");
+            string userInput = Console.ReadLine();
+            Console.WriteLine(userInput);
+            DateTime todayIs = DateTime.Today;
+            var myBdayArray = userInput.Split('/');
+            int m = int.Parse(myBdayArray[0]);
+            int d = int.Parse(myBdayArray[1]);
+            DateTime myBday = new DateTime(2016, m, d);
+            Console.WriteLine(myBday);
+            if (todayIs == myBday)
+            {
+                Console.WriteLine("Happy Birthday!");
+            }
+            else 
+            {
+                TimeSpan difference = myBday - todayIs;
+                Console.WriteLine("Your birthday is " + difference.Days + " days away!");
+            }
+            Console.ReadLine();
 
         }
     }
